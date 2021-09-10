8533ef8eebee6201019a1ad5259dd56bc40aabaa
ikaiguang <ikaiguang@163.com>
2021-09-10 18:09:25 +0800

    添加错误helper

--- error/error.kit.go
+++ error/error.kit.go
@@ -7,58 +7,51 @@ import (
 	pkgerrors "github.com/pkg/errors"
 )
 
-// Error returns an error object for the code, message.
+// Error returns an error object for the reason, message.
 func Error(reason, message string) error {
 	return pkgerrors.WithStack(errors.New(http.StatusOK, reason, message))
 }
 
-// ErrorWithMetadata returns an error object for the code, message.
+// New returns an error object for the code, reason, message.
+func New(code int, reason, message string) error {
+	return pkgerrors.WithStack(errors.New(code, reason, message))
+}
+
+// ErrorWithError returns an error object for the reason, message.
+// with an MD formed inError
+func ErrorWithError(reason, message string, inError error) error {
+	err := errors.New(http.StatusOK, reason, message)
+	if inError != nil {
+		err.Metadata = map[string]string{
+			"error": inError.Error(),
+		}
+	}
+	return pkgerrors.WithStack(err)
+}
+
+// ErrorWithMetadata returns an error object for the reason, message.
 func ErrorWithMetadata(reason, message string, md map[string]string) error {
 	err := errors.New(http.StatusOK, reason, message)
 	err.Metadata = md
 	return pkgerrors.WithStack(err)
 }
 
-// New returns an error object for the code, message.
-func New(code int, reason, message string) error {
-	return pkgerrors.WithStack(errors.New(code, reason, message))
+// NewWithError returns an error object for the code, reason, message.
+// with an MD formed inError
+func NewWithError(code int, reason, message string, inError error) error {
+	err := errors.New(code, reason, message)
+	if inError != nil {
+		err.Metadata = map[string]string{
+			"error": inError.Error(),
+		}
+	}
+	return pkgerrors.WithStack(err)
 }
 
-// NewWithMetadata returns an error object for the code, message.
+// NewWithMetadata returns an error object for the code, reason, message.
 // with an MD formed by the mapping of key, value.
 func NewWithMetadata(code int, reason, message string, md map[string]string) error {
 	err := errors.New(code, reason, message)
 	err.Metadata = md
 	return pkgerrors.WithStack(err)
-}
-
-// FromError try to convert an error to *errors.Error
-func FromError(err error) *errors.Error {
-	return errors.FromError(Cause(err))
-}
-
-// Cause returns the underlying cause of the error
-func Cause(err error) error {
-	return pkgerrors.Cause(err)
-}
-
-// Code returns the http code for a error.
-// It supports wrapped errors.
-func Code(err error) int {
-	if err == nil {
-		return 200
-	}
-	if se := FromError(err); err != nil {
-		return int(se.Code)
-	}
-	return errors.UnknownCode
-}
-
-// Reason returns the reason for a particular error.
-// It supports wrapped errors.
-func Reason(err error) string {
-	if se := FromError(err); err != nil {
-		return se.Reason
-	}
-	return errors.UnknownReason
 }
