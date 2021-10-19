5e3c70cefcf268ee6a85236a80719727a47c11c9
ikaiguang <ikaiguang@163.com>
2021-10-19 10:56:13 +0800

    添加setup_helper

---
+++ example/internal/setup/setup_helper.setup.go
@@ -0,0 +1,38 @@
+package setup
+
+import (
+	"sync"
+)
+
+var (
+	// 初始化
+	initMutex sync.Once
+	packages  Packages
+)
+
+// GetPackages 获取初始化后的Packages
+func GetPackages() (Packages, error) {
+	if err := Init(); err != nil {
+		return nil, err
+	}
+	return packages, nil
+}
+
+// Init 启动与配置与设置存储Packages
+func Init() (err error) {
+	initMutex.Do(func() {
+		packages, err = Setup()
+	})
+	if err != nil {
+		return err
+	}
+	if packages != nil {
+		return err
+	}
+
+	packages, err = Setup()
+	if err != nil {
+		return err
+	}
+	return err
+}
