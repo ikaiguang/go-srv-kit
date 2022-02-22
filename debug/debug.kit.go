c46cef8210cfef44807ea4ce76c9229699f28b8f
ikaiguang <ikaiguang@163.com>
2022-02-22 14:47:41 +0800

    添加debug-helper-fn

--- debug/debug.kit.go
+++ debug/debug.kit.go
@@ -35,6 +35,51 @@ func Debugw(keyvals ...interface{}) {
 	handler.Debugw(keyvals...)
 }
 
+// Info .
+func Info(msg interface{}) {
+	handler.Info(msg)
+}
+
+// Infof .
+func Infof(format string, a ...interface{}) {
+	handler.Infof(format, a...)
+}
+
+// Infow .
+func Infow(keyvals ...interface{}) {
+	handler.Infow(keyvals...)
+}
+
+// Warn .
+func Warn(msg interface{}) {
+	handler.Warn(msg)
+}
+
+// Warnf .
+func Warnf(format string, a ...interface{}) {
+	handler.Warnf(format, a...)
+}
+
+// Warnw .
+func Warnw(keyvals ...interface{}) {
+	handler.Warnw(keyvals...)
+}
+
+// Error .
+func Error(msg interface{}) {
+	handler.Error(msg)
+}
+
+// Errorf .
+func Errorf(format string, a ...interface{}) {
+	handler.Errorf(format, a...)
+}
+
+// Errorw .
+func Errorw(keyvals ...interface{}) {
+	handler.Errorw(keyvals...)
+}
+
 // Fatal .
 func Fatal(msg interface{}) {
 	handler.Fatal(msg)
