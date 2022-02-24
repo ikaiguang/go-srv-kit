27d01ba7af3186c7a8632df459e03bd19d2e61fd
ikaiguang <ikaiguang@163.com>
2022-02-24 20:31:28 +0800

    修改apputil工具包

--- kratos/app/app_decode.kit.go
+++ kratos/app/app_decode.kit.go
@@ -1,11 +1,14 @@
 package apputil
 
 import (
-	v1 "github.com/ikaiguang/go-srv-kit/api/response/v1"
+	"google.golang.org/protobuf/encoding/protojson"
+	"google.golang.org/protobuf/proto"
+
+	responsev1 "github.com/ikaiguang/go-srv-kit/api/response/v1"
 )
 
-// DecodeSuccessResponse 解码结果
-func DecodeSuccessResponse(contentBody []byte, data interface{}) (response *Response, err error) {
+// DecodeResponse 解码结果
+func DecodeResponse(contentBody []byte, data interface{}) (response *Response, err error) {
 	response = &Response{
 		Data: data,
 	}
@@ -16,10 +19,29 @@ func DecodeSuccessResponse(contentBody []byte, data interface{}) (response *Resp
 	return response, err
 }
 
-// DecodeErrorResponse 解码结果
-func DecodeErrorResponse(contentBody []byte) (response *v1.Response, err error) {
-	response = &v1.Response{}
+// DecodeError 解码结果
+func DecodeError(contentBody []byte) (response *responsev1.Response, err error) {
+	response = &responsev1.Response{}
 	err = UnmarshalJSON(contentBody, response)
+	if err != nil {
+		return response, err
+	}
+	return response, err
+}
+
+// DecodeProto 解码结果
+func DecodeProto(contentBody []byte, pbMessage proto.Message) (response *responsev1.Response, err error) {
+	response = &responsev1.Response{}
+	err = protojson.Unmarshal(contentBody, response)
+	if err != nil {
+		return response, err
+	}
+
+	// 解密
+	if response.Data == nil {
+		return response, err
+	}
+	err = response.Data.UnmarshalTo(pbMessage)
 	if err != nil {
 		return response, err
 	}
