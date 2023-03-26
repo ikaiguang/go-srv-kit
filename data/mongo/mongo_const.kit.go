b7bfc2aad7506faf52b06977ace0b7ff24b17561
ikaiguang <ikaiguang@163.com>
2023-03-26 18:45:12 +0800

    add 添加mongo数据库操作常量

---
+++ data/mongo/mongo_const.kit.go
@@ -0,0 +1,21 @@
+package mongoutil
+
+const (
+	OperationSet   = "$set"
+	OperationIn    = "$in"
+	OperationNotIn = "$nin"
+	OperationGte   = "$gte"
+	OperationGt    = "$gt"
+	OperationLte   = "$lte"
+	OperationLt    = "$lt"
+	OperationOr    = "$or"
+	OperationUnset = "$unset"
+	OperationRegex = "$regex"
+	OperationInc   = "$inc"
+	OperationPull  = "$pull"
+	OperationNin   = "$nin"
+	OperationNe    = "$ne"
+	OperationEq    = "$eq"
+	OperationMatch = "$match"
+	OperationGroup = "$group"
+)
