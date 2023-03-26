ef362076405abd940760fa7abd357941f1156f20
ikaiguang <ikaiguang@163.com>
2023-03-26 18:44:43 +0800

    add 添加方法md5util.FileMd5()

--- kit/md5/md5.kit.go
+++ kit/md5/md5.kit.go
@@ -3,6 +3,8 @@ package md5util
 import (
 	"crypto/md5"
 	"encoding/hex"
+	"io"
+	"os"
 )
 
 // Md5 .
@@ -14,4 +16,21 @@ func Md5(bodyBytes []byte) (res string, err error) {
 	}
 	res = hex.EncodeToString(handler.Sum(nil))
 	return res, err
+}
+
+// FileMd5 获取文件的MD5
+func FileMd5(path string) (string, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return "", err
+	}
+	defer func() { _ = f.Close() }()
+
+	h := md5.New()
+	_, err = io.Copy(h, f)
+	if err != nil {
+		return "", err
+	}
+
+	return hex.EncodeToString(h.Sum(nil)), nil
 }
