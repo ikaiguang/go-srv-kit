b5c1f1de3a8dc9b58a3109214fe27ca0df022451
chenkaiguang <chenkaiguang@realibox.com>
2024-06-17 16:55:50 +0800

    feat: add gormpkg.InitPaginatorArgs

--- data/gorm/gorm_page.kit.go
+++ data/gorm/gorm_page.kit.go
@@ -8,12 +8,26 @@ import (
 
 // PaginatorArgs 列表参数
 type PaginatorArgs struct {
+	// PageRequest 分页
+	PageRequest *pagepkg.PageRequest
 	// PageOption 分页
 	PageOption *pagepkg.PageOption
 	// PageOrders 排序
 	PageOrders []*Order
 	// PageWheres 条件
 	PageWheres []*Where
+}
+
+func InitPaginatorArgs(page, pageSize uint32) *PaginatorArgs {
+	res := &PaginatorArgs{}
+	res.PageRequest, res.PageOption = pagepkg.ParsePageRequestArgs(page, pageSize)
+	return res
+}
+
+func InitPaginatorArgsByPageRequest(pageRequest *pagepkg.PageRequest) *PaginatorArgs {
+	res := &PaginatorArgs{}
+	res.PageRequest, res.PageOption = pagepkg.ParsePageRequest(pageRequest)
+	return res
 }
 
 // Paginator 分页
