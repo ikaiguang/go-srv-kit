f80a2634ab45d96c0dad9a83128bdbc2acec7041
chenkaiguang <chenkaiguang@realibox.com>
2024-08-16 11:33:45 +0800

    feat: mongo logger

--- data/mongo/mongo_log.kit.go
+++ data/mongo/mongo_log.kit.go
@@ -2,8 +2,6 @@ package mongopkg
 
 import (
 	"context"
-	"strconv"
-
 	"github.com/go-kratos/kratos/v2/log"
 	"go.mongodb.org/mongo-driver/event"
 )
@@ -13,23 +11,18 @@ func NewMonitor(logger log.Logger) *event.CommandMonitor {
 	return &event.CommandMonitor{
 		Started: func(ctx context.Context, evt *event.CommandStartedEvent) {
 			_ = log.WithContext(ctx, logger).Log(log.LevelDebug,
-				"request_id",
-				evt.RequestID,
-				"database",
-				evt.DatabaseName,
-				"command_name",
-				evt.CommandName,
-				"query",
-				evt.Command.String(),
+				"request_id", evt.RequestID,
+				"database", evt.DatabaseName,
+				"command_name", evt.CommandName,
+				"command_script", evt.Command.String(),
 			)
 		},
 		Succeeded: func(ctx context.Context, evt *event.CommandSucceededEvent) {
-			d := evt.DurationNanos / (1000 * 1000)
 			_ = log.WithContext(ctx, logger).Log(log.LevelDebug,
 				"request_id", evt.RequestID,
 				"command_name", evt.CommandName,
+				"exec_duration", evt.Duration.String(),
 				// "result", evt.Reply.String(),
-				"duration", strconv.FormatInt(d, 10)+"ms",
 			)
 		},
 	}
