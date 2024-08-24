a9d2a2240ccfdee76ff1ccdf25ae6ba6e33eeb7d
ikaiguang <ikaiguang@163.com>
2024-08-24 22:08:35 +0800

    feat: IsErrRecordNotFound

--- data/gorm/gorm_helper.kit.go
+++ data/gorm/gorm_helper.kit.go
@@ -1,6 +1,7 @@
 package gormpkg
 
 import (
+	stderrors "errors"
 	"time"
 
 	"gorm.io/gorm"
@@ -14,12 +15,37 @@ const (
 	FieldIsDeleted   = "is_deleted"
 )
 
+var (
+	_ = gorm.Model{}
+)
+
 type Model struct {
 	Id          uint64    `gorm:"column:id;type:uint;autoIncrement;comment:ID" json:"id"`
 	CreatedTime time.Time `gorm:"column:created_time;type:time;not null;comment:创建时间" json:"created_time"`
 	UpdatedTime time.Time `gorm:"column:updated_time;type:time;not null;comment:更新时间" json:"updated_time"`
-	IsDelete    bool      `gorm:"column:is_deleted;type:uint;default:0;comment:是否删除" json:"is_deleted"`
+	IsDeleted   bool      `gorm:"column:is_deleted;type:uint;default:0;comment:是否已删除" json:"is_deleted"`
 	DeletedTime time.Time `gorm:"column:deleted_time;type:time;comment:删除时间" json:"deleted_time"`
+}
+
+type ModelForMysql struct {
+	Id          uint64    `gorm:"column:id;type:uint;autoIncrement;default:current_timestamp();comment:ID" json:"id"`
+	CreatedTime time.Time `gorm:"column:created_time;type:time;not null;default:current_timestamp();comment:创建时间" json:"created_time"`
+	UpdatedTime time.Time `gorm:"column:updated_time;type:time;not null;default:current_timestamp();comment:更新时间" json:"updated_time"`
+	IsDeleted   bool      `gorm:"column:is_deleted;type:uint;default:0;comment:是否已删除" json:"is_deleted"`
+	DeletedTime time.Time `gorm:"column:deleted_time;type:time;comment:删除时间" json:"deleted_time"`
+}
+
+type ModelForPostgres struct {
+	Id          uint64    `gorm:"column:id;type:uint;autoIncrement;default:current_timestamp;comment:ID" json:"id"`
+	CreatedTime time.Time `gorm:"column:created_time;type:time;not null;default:current_timestamp;comment:创建时间" json:"created_time"`
+	UpdatedTime time.Time `gorm:"column:updated_time;type:time;not null;default:current_timestamp;comment:更新时间" json:"updated_time"`
+	IsDeleted   bool      `gorm:"column:is_deleted;type:uint;default:0;comment:是否已删除" json:"is_deleted"`
+	DeletedTime time.Time `gorm:"column:deleted_time;type:time;comment:删除时间" json:"deleted_time"`
+}
+
+// IsErrRecordNotFound ...
+func IsErrRecordNotFound(err error) bool {
+	return stderrors.Is(err, gorm.ErrRecordNotFound)
 }
 
 // QueryUndeletedData 未删除的数据
