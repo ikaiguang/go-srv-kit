2c255776d79b43d6c184e489cc9f4664f629a499
ikaiguang <ikaiguang@163.com>
2024-09-06 23:26:07 +0800

    feat: 更新auth.error定义

--- kratos/auth/auth_error.kit.go
+++ kratos/auth/auth_error.kit.go
@@ -11,44 +11,44 @@ func Is(err, target error) bool {
 }
 
 func ErrMissingToken() *errors.Error {
-	return errors.Unauthorized(ERROR_TOKEN_MISSING.String(), "token is missing")
+	return ErrorTokenMissing("token is missing")
 }
 func ErrMissingSignKeyFunc() *errors.Error {
-	return errors.Unauthorized(ERROR_TOKEN_KEY_MISSING.String(), "keyFunc is missing")
+	return ErrorTokenKeyMissing("keyFunc is missing")
 }
 func ErrUnSupportSigningMethod() *errors.Error {
-	return errors.Unauthorized(ERROR_TOKEN_METHOD_MISSING.String(), "Wrong signing method")
+	return ErrorTokenMethodMissing("Wrong signing method")
 }
 func ErrTokenParseFail() *errors.Error {
-	return errors.Unauthorized(ERROR_AUTHENTICATION_FAILED.String(), "Fail to parse JWT token ")
+	return ErrorAuthenticationFailed("Fail to parse JWT token ")
 }
 func ErrTokenExpired() *errors.Error {
-	return errors.Unauthorized(ERROR_TOKEN_EXPIRED.String(), "JWT token has expired")
+	return ErrorTokenExpired("JWT token has expired")
 }
 func ErrWrongContext() *errors.Error {
-	return errors.Unauthorized(ERROR_UNAUTHORIZED.String(), "Wrong context for middleware")
+	return ErrorUnauthorized("Wrong context for middleware")
 }
 func ErrTokenInvalid() *errors.Error {
-	return errors.Unauthorized(ERROR_TOKEN_INVALID.String(), "Token is invalid")
+	return ErrorTokenInvalid("Token is invalid")
 }
 func ErrNeedTokenProvider() *errors.Error {
-	return errors.Unauthorized(ERROR_TOKEN_INVALID.String(), "Token provider is missing")
+	return ErrorTokenInvalid("Token provider is missing")
 }
 func ErrSignToken() *errors.Error {
-	return errors.Unauthorized(ERROR_TOKEN_INVALID.String(), "Can not sign token.Is the key correct?")
+	return ErrorTokenInvalid("Can not sign token.Is the key correct?")
 }
 func ErrGetKey() *errors.Error {
-	return errors.Unauthorized(ERROR_TOKEN_INVALID.String(), "Can not get key while signing token")
+	return ErrorTokenInvalid("Can not get key while signing token")
 }
 func ErrInvalidAuthToken() *errors.Error {
-	return errors.Unauthorized(ERROR_VERIFICATION_FAILED.String(), "[validator] Invalid auth token")
+	return ErrorVerificationFailed("[validator] Invalid auth token")
 }
 func ErrInvalidClaims() *errors.Error {
-	return errors.Unauthorized(ERROR_INVALID_CLAIMS.String(), "[validator] Invalid auth claims")
+	return ErrorInvalidClaims("[validator] Invalid auth claims")
 }
 func ErrBlacklist() *errors.Error {
-	return errors.Unauthorized(ERROR_TOKEN_DEPRECATED.String(), "[validator] Deprecated token")
+	return ErrorTokenDeprecated("[validator] Deprecated token")
 }
 func ErrWhitelist() *errors.Error {
-	return errors.Unauthorized(ERROR_TOKEN_NOT_IN_WHITELIST.String(), "[validator] The token is not in the valid list")
+	return ErrorTokenNotInWhitelist("[validator] The token is not in the valid list")
 }
