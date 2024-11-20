455cad53dfbdb22e6cdff287ff9d8b5b0ca9f5da
chenkaiguang <chenkaiguang@realibox.com>
2024-11-20 09:56:57 +0800

    feat: gen nodeid

--- kit/id/id.kit.go
+++ kit/id/id.kit.go
@@ -1,5 +1,11 @@
 package idpkg
 
+import (
+	"fmt"
+	ippkg "github.com/ikaiguang/go-srv-kit/kit/ip"
+	"net"
+)
+
 var (
 	// Node 生成ID的节点
 	// 为了帮助保证唯一性
@@ -13,8 +19,11 @@ var (
 // BenchmarkNew_BwmarrinSnowflake-8		76981             15611 ns/op               0 B/op          0 allocs/op
 // ===== Benchmark =====
 func init() {
-	var err error
-	Node, err = NewBwmarrinSnowflake(1)
+	nodeID, err := GenNodeID()
+	if err != nil {
+		nodeID = 1
+	}
+	Node, err = NewBwmarrinSnowflake(int64(nodeID))
 	if err != nil {
 		panic(err)
 	}
@@ -30,4 +39,21 @@ func SetNode(node Snowflake) {
 // - 确保您永远不会有多个节点以相同的节点 ID 运行
 func NextID() (uint64, error) {
 	return Node.NextID()
+}
+
+func GenNodeID() (uint16, error) {
+	return IPV4ToNodeID(ippkg.LocalIP())
+}
+
+func IPV4ToNodeID(ip string) (uint16, error) {
+	ipAddr := net.ParseIP(ip)
+	if ipAddr == nil {
+		return 0, fmt.Errorf("invalid IP address: %s", ip)
+	}
+
+	var lastTwoBytes [2]byte
+	copy(lastTwoBytes[:], ipAddr.To4()[2:])
+
+	nodeId := uint16(lastTwoBytes[0])<<8 | uint16(lastTwoBytes[1])
+	return nodeId, nil
 }
