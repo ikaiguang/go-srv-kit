be82be13800b1b7391c85d25e98b689937892492
chenkaiguang <kaiguangchan@gmail.com>
2024-12-12 18:55:49 +0800

    feat: build script

--- testdata/ping-service/devops/makefile_cicd.mk
+++ testdata/ping-service/devops/makefile_cicd.mk
@@ -36,6 +36,30 @@ build:
 		-t ping-service:latest \
 		-f ./testdata/ping-service/devops/docker-build/Dockerfile .
 
+.PHONY: deploy-general-config
+# deploy :-->: store general config
+deploy-general-config:
+	@echo "deploy :-->: general config"
+	go run ./testdata/ping-service/cmd/store-configuration/... \
+      -conf=./testdata/ping-service/configs \
+      -source_dir ./testdata/ping-service/devops/docker-deploy/general-configs \
+      -store_dir go-micro-saas/general-configs/testing
+
+.PHONY: deploy-service-config
+# deploy :-->: store service config
+deploy-service-config:
+	@echo "deploy :-->: service config"
+	go run ./testdata/ping-service/cmd/store-configuration/... \
+      -conf=./testdata/ping-service/configs \
+      -source_dir ./testdata/ping-service/devops/docker-deploy/service-configs \
+      -store_dir go-micro-saas/ping-service/testing/v1.0.0
+
+.PHONY: deploy-database-migration
+# deploy :-->: database migration
+deploy-database-migration:
+	@echo "deploy :-->: database migration"
+	$(MAKE) run-database-migration
+
 # deploy-image on docker
 .PHONY: deploy-on-docker
 # deploy-on-docker :-->: deploying on docker
