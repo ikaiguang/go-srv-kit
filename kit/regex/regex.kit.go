ed9101d9730ab9bba0f5f07c7f26206169797ab2
chenkaiguang <chenkaiguang@realibox.com>
2024-12-20 17:44:10 +0800

    feat: regexpkg

--- kit/regex/regex.kit.go
+++ kit/regex/regex.kit.go
@@ -5,8 +5,10 @@ import (
 )
 
 var (
-	phoneRegex = regexp.MustCompile(`^1[1-9]\d{9}$`)
-	emailRegex = regexp.MustCompile(`^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`)
+	phoneRegex    = regexp.MustCompile(`^1[1-9]\d{9}$`)
+	emailRegex    = regexp.MustCompile(`^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`)
+	idCardRegex   = regexp.MustCompile(`^(\d{17}[\dX]|\d{15})$`)
+	postCodeRegex = regexp.MustCompile(`^\d{6}$`)
 )
 
 func IsValidPhone(phone string) bool {
@@ -15,4 +17,12 @@ func IsValidPhone(phone string) bool {
 
 func IsValidEmail(email string) bool {
 	return emailRegex.MatchString(email)
+}
+
+func IsIDCard(id string) bool {
+	return idCardRegex.MatchString(id)
+}
+
+func IsPostCode(code string) bool {
+	return postCodeRegex.MatchString(code)
 }
